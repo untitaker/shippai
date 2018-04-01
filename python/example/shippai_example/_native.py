46ac461eb636239de953945a33a97d6284d51fd2
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-04-01 12:17:59 +0200

    Remove generated files from git

--- python/example/shippai_example/_native.py
+++
@@ -1,9 +0,0 @@
-# auto-generated file
-__all__ = ['lib', 'ffi']
-
-import os
-from shippai_example._native__ffi import ffi
-
-lib = ffi.dlopen(os.path.join(
-    os.path.dirname(__file__), '_native__lib.so'), 4096)
-del os
