84db8adf4bb99975089525a57edd01327516889a
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-05-08 20:11:04 +0200

    Version 0.2.4

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='shippai',
-    version='0.2.3',
+    version='0.2.4',
     description='Interface with Rust errors seamlessly',
     long_description=open('README.rst').read(),
     author='Markus Unterwaditzer',
